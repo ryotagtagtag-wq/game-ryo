37bfae92e1fa62fe58ce93e12e73b8e3b42015e6
game_ryo <ryotagtagtag@gmail.com>
2026-09-19 07:15:11 +0900

    chore: rename 404 to njk and log easter egg
    - Converted static 404.html to 404.njk template
    - Moved hidden message from HTML comment to console
    - Removed 404.html from Eleventy passthrough config
    
    Signed-off-by: game_ryo <ryotagtagtag@gmail.com>

--- eleventy.config.js
+++ eleventy.config.js
@@ -201,7 +201,7 @@ module.exports = function (eleventyConfig) {
   });
 
   // パススルー
-  ["CNAME", "_redirects", "_headers", "css", "images", "js", "fonts", ".well-known", "404.html"]
+  ["CNAME", "_redirects", "_headers", "css", "images", "js", "fonts", ".well-known"]
     .forEach(p => eleventyConfig.addPassthroughCopy(p));
 
   return {
